610b89b8f02ae6071243e37cab0074ae6fa0f06b
Reshit Fazlija <reshitf@gmail.com>
2021-02-22 04:27:06 +0100

    Optimised code

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
 import csv
-f = open('first-names.json')
 class Customer:
     def __init__(self, name, membership_type):
         self.name = name
@@ -12,17 +11,13 @@ class Customer:
 customers = []
 membership_types = ["Bronze", "Silver", "Gold", "Platinum", "Diamond"]
 from random import randrange
-names = json.load(f)
-countagain = 0
+names = json.load(open('first-names.json'))
 for i in names:
-    customers.append(Customer(names[countagain], membership_types[randrange(5)]))
-    countagain = countagain + 1
+    customers.append(Customer(names[names.index(i)], membership_types[randrange(5)]))
 
-count = 0
 with open('person.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     for j in customers:
-        writer.writerow([customers[count].name, customers[count].membership_type])
-        count = count + 1
+        writer.writerow([customers[customers.index(j)].name, customers[customers.index(j)].membership_type])
 
 
