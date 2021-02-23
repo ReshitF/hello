5efc2846ac4239c3021f705f171d0d61e668ea4c
Reshit Fazlija <reshitf@gmail.com>
2021-02-23 16:00:17 +0100

    Yeet

--- main2.py
+++
@@ -1,8 +0,0 @@
-from random import randrange
-number = randrange(10)+1
-print(number)
-poop = input("Guess a number:")
-if number == poop:
-    print("pog")
-elif number != poop:
-    print ('not pog')
